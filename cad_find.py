a6f0f8feba3acc741fc7e034005c3531c48d779d
Dawid Huczyński <tibicen@tlen.pl>
2016-11-15 15:31:12 +0100

    adds area2xls func

--- cad_find.py
+++ cad_find.py
@@ -1,59 +1,74 @@
+# -*- coding: utf-8 -*-
+import datetime
 import os
 import sys
 from multiprocessing import Pool
 
 import ezdxf
 
+WORK_DIR = 'C:\\test'
 FILES_DWG = ['BSH-LAB_QMT-PW-A.dwg']
 FILES_DWG = sys.argv[1:] if len(sys.argv) > 1 else FILES_DWG
+ACAD_PATH = None
 
 
-def find():
+def find_acad():
+    global ACAD_PATH
+    if ACAD_PATH:
+        return ACAD_PATH
     for p, d, f in os.walk('c:\\'):
         if 'acad.exe' in f:
-            print(p + '\\acad.exe')
+            ACAD_PATH = p + '\\acad.exe'
             return (p + '\\acad.exe')
         if 'acadlt.exe' in f:
-            print(p + '\\acadlt.exe')
+            ACAD_PATH = p + '\\acadlt.exe'
             return (p + '\\acadlt.exe')
 
 
-def createScript(FILE_DWG):
-    if FILE_DWG.rstrip('dwg') + 'dxf' in os.listdir():
-        QUIT = '\nY\nQUIT\nY\n'
-    else:
-        QUIT = '\nQUIT\nY\n'
-    SCRIPT = 'FILEDIA\n0\nPURGE\nA\n*\nN\nAUDIT\nY\nSAVEAS\nDXF\n16\n' + \
-        os.path.join(os.getcwd(), FILE_DWG.rstrip('dwg') + 'dxf') + QUIT
+def openDrawning(FILE):
+    if FILE.endswith('dwg'):
+        if FILE.rstrip('dwg') + 'dxf' not in os.listdir():
+            createDXF(FILE)
+        return ezdxf.readfile(FILE.rstrip('dwg') + 'dxf')
+    elif FILE.endswith('dxf'):
+        return ezdxf.readfile(FILE)
+
+
+def createScript(FILE):
+    # SCRIPT = 'FILEDIA\n0\nPURGE\nA\n*\nN\nAUDIT\nY\nSAVEAS\nDXF\n16\n' + \
+    #     os.path.join(os.getcwd(), FILE.rstrip('dwg') + 'dxf') + "\nQUIT"
     SCRIPT = 'FILEDIA\n0\nSAVEAS\nDXF\n16\n' + \
-        os.path.join(os.getcwd(), FILE_DWG.rstrip('dwg') + 'dxf') + QUIT
+        os.path.join(os.getcwd(), FILE.rstrip('dwg') + 'dxf') + "\nQUIT\n\n"
     tmpDWG = open('SCRIPT.scr', 'w')
     tmpDWG.write(SCRIPT)
     tmpDWG.close()
 
 
-def runDWG(FILE_DWG, AcadExePath):
+def runDWG(FILE, AcadExePath):
     tmpDir = os.getcwd()
     os.chdir(os.path.split(AcadExePath)[0])
     os.system(os.path.split(AcadExePath)[1] + ' "' + os.path.join(
-        tmpDir, FILE_DWG) + '" /nologo /nossm /b "' + os.path.join(
+        tmpDir, FILE) + '" /nologo /nossm /b "' + os.path.join(
         tmpDir, 'SCRIPT.scr') + '"')
     os.chdir(tmpDir)
     os.remove('SCRIPT.scr')
 
 
-def createDXF(FILE_DWG, SCRIPT):
-    print('creating dxf for ', FILE_DWG)
-    AcadExePath = find()
-    createScript(FILE_DWG)
-    runDWG(FILE_DWG, AcadExePath)
+def createDXF(FILE):
+    print('creating dxf for ', FILE)
+    AcadExePath = find_acad()
+    createScript(FILE)
+    if FILE.rstrip('dwg') + 'dxf' in os.listdir():
+        os.remove(FILE.rstrip('dwg') + 'dxf')
+    runDWG(FILE, AcadExePath)
+    os.remove(FILE.rstrip('dwg') + 'bak')
 
 
 def job(z):
-    nr, FILE_DWG, RYSUNKI = z
-    createDXF(FILE_DWG)
-    print('opening ', FILE_DWG.rstrip('dwg') + 'dxf')
-    dxf = ezdxf.readfile(FILE_DWG.rstrip('dwg') + 'dxf')
+    nr, FILE, RYSUNKI = z
+    createDXF(FILE)
+    print('opening ', FILE.rstrip('dwg') + 'dxf')
+    dxf = ezdxf.readfile(FILE.rstrip('dwg') + 'dxf')
     tabelki = dxf.entities.query('INSERT[name=="TABELKA"]')
     for tab in tabelki:
         NR = tab.get_attrib_text('NR_RYSUNKI')
@@ -64,7 +79,7 @@ def job(z):
                 NAZWA += attr.dxf.text + ' '
         RYSUNKI.append((NR, SKALA, NAZWA.rstrip(' ')))
         print("'{}','{}','{}'".format((NR, SKALA, NAZWA.rstrip(' '))))
-    os.remove(FILE_DWG.rstrip('dwg') + 'dxf')
+    os.remove(FILE.rstrip('dwg') + 'dxf')
 
 
 def main():
@@ -78,5 +93,48 @@ def main():
         print("'{}','{}','{}'".format(*rys))
     return RYSUNKI
 
+
+def sum_all_areas():
+    dxf = ezdxf.readfile('C:\\test\\bb.dxf')
+    texts = dxf.entities.query('MTEXT')
+    texts = [x for x in texts if x.dxf.layer == 'R2_POWIERZCHNIE']
+    l4 = [x for x in texts if x.dxf.insert[0] > 40000]
+
+
+def areas2xls4dabki():
+    files = []
+    l = len(os.listdir())
+    for nr, f in enumerate(os.listdir()):
+        if f.endswith('.dwg'):
+            files.append(openDrawning(f))
+            print('\rOpening files... remains {:>2}'.format(l - nr), end='')
+    print('\r')
+    metki = []
+    for dxf in files:
+        tmp_metki = dxf.entities.query('INSERT[name=="METKA2"]')
+        metki += list(tmp_metki)
+    metki = sorted(metki, key=lambda x: x.get_attrib_text('NR'))
+    today = datetime.date.today().isoformat()
+    f = open('DĄBKI ZESTAWIENIE POWIERZCHNI {}.csv'.format(today),
+             'w', encoding='utf-8')
+    for nr, m in enumerate(metki):
+        float_error = False
+        NR = m.get_attrib_text('NR')
+        try:
+            POW = float(m.get_attrib_text('POW').replace(',', '.'))
+        except(ValueError):
+            POW = m.get_attrib_text('POW').replace(',', '.')
+            float_error = True
+        NAZWA = m.get_attrib_text('NAZWA_POM')
+        if not float_error:
+            print('{},\t{:>6.2f}m2,\t{}'.format(NR, POW, NAZWA))
+            f.write("{},{:.2f},{}\n".format(NR, POW, NAZWA))
+        else:
+            print('{},\t{}m2,\t{}'.format(NR, POW, NAZWA))
+            f.write("{},{},{}\n".format(NR, POW, NAZWA))
+    f.close()
+
+
 if __name__ == '__main__':
-    RYSUNKI = main()
+    os.chdir(WORK_DIR)
+    # areas2xls4dabki()
